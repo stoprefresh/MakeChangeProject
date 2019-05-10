26baa1eb095cad2efcdee9c23295c4759428f825
Miguel <marsigliamiguel@protonmail.com>
2019-05-10 17:32:56 -0600

    Added a main method with comments.

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -1,4 +1,12 @@
 
 public class MakeChangeApp {
 
+		public static void main(String[] args) {
+			
+			//Prompt user for value
+			
+			//Prompt user for amount tendered, and get input
+			
+			
+		}
 }
