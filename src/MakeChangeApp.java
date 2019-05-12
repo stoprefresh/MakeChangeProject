a7f222dd77890c3aae975c3d4ff64cb6b5dabc11
Miguel <marsigliamiguel@protonmail.com>
2019-05-12 09:31:05 -0500

    errorCheck & return methods started

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -7,6 +7,7 @@ public class MakeChangeApp {
 			Scanner kb = new Scanner(System.in);
 			
 			double tendered = 0.0, productCost= 0.0, total;
+			boolean tranApprov;
 			
 			System.out.println("Enter the cost of a product: ");
 			productCost = kb.nextDouble();
@@ -14,35 +15,53 @@ public class MakeChangeApp {
 			System.out.println("Enter the amount tendered: ");
 			tendered = kb.nextDouble();
 			
-			total = returnCalc(tendered, productCost);
+			
+			tranApprov = errorCheck(tendered, productCost);
+			total = returnCalc(tendered, productCost, tranApprov);
 			
 			//Prompt user for value
-			
+			currencyReturned(tendered, tranApprov);
 			//Prompt user for amount tendered, and get input
-			
 			//This will take sometime to complete.
 			
 			kb.close();
 		}
-		
-		public static double returnCalc(double tendered, double productCost) {
+		// This method will determine how much currency is returned
+		public static double returnCalc(double tendered, double productCost, boolean tranApprov) {
 			double total = 0.0;
 			
+			if (tranApprov = true && tendered > productCost) {
+				total = Math.abs(productCost - tendered);
+				System.out.printf("%n$%.2f" + total);
+			}
+			else if(tranApprov = true && tendered == productCost){
+				System.out.println("You have paid the amount in its entirety!");
+				System.out.println("Thank you for purchasing!");
+			}
 			
 			
 			
 			return total;
 		}
-		
-		public static void currencyReturned(double tendered) {
+		// currencyReturned will determine how much of each denomination is returned
+		public static void currencyReturned(double tendered, boolean tranApprov) {
 			
 			
 			
 		}
-		
+		// errorCheck ensures that the customer has given paid enough to proceed
 		public static boolean errorCheck(double tendered, double productCost) {
-			boolean transApprov = false;
+			boolean tranApprov = false;
 			
-			return transApprov;
+			if(tendered < productCost) {
+				tranApprov = false;
+				System.out.println("Insufficient funds to complete this transaction.");
+			}
+			else if(tendered >= productCost) {
+				tranApprov = true;
+				System.out.println("Thank you!");
+			}
+			
+			return tranApprov;
 		}
 }
